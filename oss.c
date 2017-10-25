24863b97329820ec253e83edec8e0d66a0917492
SandhyaVaidyanathan <o2-vaidyanathan@hoare7.cs.umsl.edu>
2017-10-25 16:01:36 -0500

    shm.h 	user.c struct

---
+++ oss.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <getopt.h>
+
+#include "shm.h"
+
+int main(int argc, char const *argv[])
+{
+	char* logfile;
+	logfile = "log.txt";
+
+
+
+	return 0;
+}
